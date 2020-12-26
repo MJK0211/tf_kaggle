d910a4272743f7625c81de18b52ad4a4a573dcfa
MJK0211 <kimminjong0211@gmail.com>
2020-12-27 00:16:26 +0900

    12_26_commit

--- 1_Titanic/code/titanic_1_data.py
+++ 1_Titanic/code/titanic_1_data.py
@@ -4,19 +4,19 @@ import numpy as np
 import pandas as pd
 
 #1. 데이터
-gender = pd.read_csv('./data/csv/gender_submission.csv',
+gender = pd.read_csv('./1_Titanic/data/csv/gender_submission.csv',
                      index_col=0,
                      header=0,
                      encoding='UTF-8'
 )
 
-train = pd.read_csv('./data/csv/train.csv',
+train = pd.read_csv('./1_Titanic/data/csv/train.csv',
                      index_col=0,
                      header=0,
                      encoding='UTF-8'                 
 )
 
-test = pd.read_csv('./data/csv/train.csv',
+test = pd.read_csv('./1_Titanic/data/csv/test.csv',
                      index_col=0,
                      header=0,
                      encoding='UTF-8'
@@ -24,7 +24,7 @@ test = pd.read_csv('./data/csv/train.csv',
 
 print(gender.shape) #(418, 1)
 print(train.shape) #(891, 11)
-print(test.shape) #(891, 11)
+print(test.shape) #(418, 10)
 
 print(train.isnull().sum())
 # Survived      0
@@ -53,32 +53,14 @@ print(test.isnull().sum())
 # Cabin       687
 # Embarked      2
 # dtype: int64
-# Survived      0
 # Pclass        0
 # Name          0
 # Sex           0
-# Age         177
+# Age          86
 # SibSp         0
 # Parch         0
 # Ticket        0
-# Fare          0
-# Cabin       687
-# Embarked      2
+# Fare          1
+# Cabin       327
+# Embarked      0
 # dtype: int64
-
-
-'''
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(style='darkgrid')
-
-def bar_chart(feature):
-    survived = train[train['Survived']==1][feature].value_counts()
-    dead = train[train['Survived']==0][feature].value_counts()
-    df = pd.DataFrame([survived,dead])
-    df.index = ['Survived', 'Dead']
-    df.plot(kind='bar', stacked=True, figsize=(10,5))
-    plt.show()
-
-# bar_chart('Sex')
-'''
---
+++ 1_Titanic/code/titanic_3_test.py
@@ -0,0 +1,39 @@
+
+
+import numpy as np
+import pandas as pd
+
+#1. 데이터
+gender = pd.read_csv('./1_Titanic/data/csv/gender_submission.csv',
+                     index_col=0,
+                     header=0,
+                     encoding='UTF-8'
+)
+
+train = pd.read_csv('./1_Titanic/data/csv/train.csv',
+                     index_col=0,
+                     header=0,
+                     encoding='UTF-8'                 
+)
+
+test = pd.read_csv('./1_Titanic/data/csv/test.csv',
+                     index_col=0,
+                     header=0,
+                     encoding='UTF-8'
+)
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='darkgrid')
+
+def bar_chart(feature):
+    survived = train[train['Survived']==1][feature].value_counts()
+    dead = train[train['Survived']==0][feature].value_counts()
+    df = pd.DataFrame([survived,dead])
+    df.index = ['Survived', 'Dead']
+    df.plot(kind='bar', stacked=True, figsize=(10,5))
+    plt.show()
+
+bar_chart('Sex')
+
